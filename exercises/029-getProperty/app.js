a44ef094677fccf4844d9d83bbbef27643b7a6e0
Federico ferreyra <128550575+fedepaino@users.noreply.github.com>
2025-02-25 09:09:54 +0000

    se resolvionhasta el ejercicio nº 48

--- exercises/029-getProperty/app.js
+++ exercises/029-getProperty/app.js
@@ -1,4 +1,11 @@
+let car = {
+  model: 'Toyota'
+};
+
 function getProperty(obj, key) {
   // your code here
-  
+  return obj[key];
 }
+
+let output = getProperty(car, 'model');
+console.log(output); // --> 'Toyota'
