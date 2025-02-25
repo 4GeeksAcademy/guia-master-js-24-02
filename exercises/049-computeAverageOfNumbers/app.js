f54ce65967dc09562680a2d72cb6ee5f9d752933
Federico ferreyra <128550575+fedepaino@users.noreply.github.com>
2025-02-25 13:34:28 +0000

    ejercicios reueltos hasta el nº 60

--- exercises/049-computeAverageOfNumbers/app.js
+++ exercises/049-computeAverageOfNumbers/app.js
@@ -1,1 +1,11 @@
 // Write your function here
+function computeAverageOfNumbers(arr) {
+    if(arr.length === 0) {
+        return 0;
+    }
+    let sumaArr = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sumaArr += arr[i];
+    }
+    return sumaArr / arr.length;
+}
